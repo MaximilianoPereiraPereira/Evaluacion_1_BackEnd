b915d424791d61a25cc49816b6dc60f45ffa9bda
Laboratorio 117 <CAL_LAB117@inacap.cl>
2026-09-14 11:28:24 -0300

    Çreacion evaluacion 1 y paginas

---
+++ paginas_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def mostrar_home(request):
+    return render(request, 'index.html')
+
+def mostrar_servicio(request):
+    card = {
+        "Servicio": "Lavado de Vehiculos",
+        "Valor": 10000
+    }
+
+    return render(request, 'servicio.html', card)
